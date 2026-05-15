f32548b36dd27c86efb50a2cd4f80356a1038fd2
alexzlzhang <alexzlzhang@tencent.com>
2026-05-15 17:26:26 +0800

    newLearn

--- review/test.go
+++ review/test.go
@@ -1,14 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var uidList []string
-	fmt.Println(uidList)
-	if len(uidList) == 0 {
-		fmt.Println(2)
-	}
-	if uidList == nil {
-		fmt.Println(1)
-	}
+
 }
